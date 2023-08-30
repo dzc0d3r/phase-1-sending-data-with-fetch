ed4d63d32c5ad32ea6defed37267419787a9c13e
dzc0d3r <m4dcod3r@protonmail.com>
2023-08-30 22:28:39 +0100

    Completed assignements :tada:

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users'; // The URL for the POST request
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }),
+    }
+
+
+    return fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(errorMessage => {
+                    throw new Error(errorMessage);
+                });
+            }
+
+            return response.json();
+
+        })
+        .then(responseData => {
+            const newId = responseData.id;
+            document.body.innerHTML += `<p>New ID: ${newId}</p>`;
+        })
+        .catch(error => {
+            document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+        });
+}
